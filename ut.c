055ef2e8ee25e1a55efbe841f7ea489628334736
utkarshvinaik <vinaik.utkarsh2007@gmail.com>
2025-08-23 18:35:53 +0530

    Add files via upload

---
+++ ut.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+
+    int a ;
+    
+    printf("enter a number a");
+    scanf("%d", &a);
+    int b ; 
+    printf("eneter a number b ");
+    scanf("%d", &b);
+    printf("the sum of a and b is %d", a+b);
+    return 0;
+}
